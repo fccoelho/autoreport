031b0f3adcfbf4e32f2b565d7b5fe120187112a2
Flávio Codeço Coelho <fccoelho@gmail.com>
2023-04-26 09:17:27 -0300

    adicionei testes

--- relatorio/cria.py
+++ relatorio/cria.py
@@ -1,14 +1,16 @@
 
-
+import os
 
 class Relatorio:
-    def __init__(self, titulo, autor) -> None:
+    template_dir = "dados"#os.path.join("..", "dados")
+    def __init__(self, titulo, autor, template='cabeçalho.tex') -> None:
+        self.template = template
         self.cabeçalho=None
         self.titulo = titulo
         self.autor = autor
         self.inicializa()
 
     def inicializa(self):
-        with open('..dados/cabeçalho.tex') as f:
+        with open(os.path.join(self.template_dir,'cabeçalho.tex')) as f:
             self.cabeçalho = f.read()
 
